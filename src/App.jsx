b5f19748f8de7fcb42eb4cd4851e59c8599fe0e1
Suzy-Lana777 <reutasvetlana382@gmail.com>
2025-05-08 08:10:33 +0300

    step_6

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Feedback from './components/Feedback/Feedback';
 import Options from './components/Options/Options';
 import Notification from './components/Notification/Notification';
 import Description from './components/Description/Description';
 
 export default function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+  const [feedback, setFeedback] = useState(
+    JSON.parse(localStorage.getItem('feedback')) ?? {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    },
+  );
+
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
+
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
 
   const updateFeedback = (feedbackType) => {
     if (feedbackType === 'reset') {
